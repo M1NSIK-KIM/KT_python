3c8e3e50a45070f95fd398fb9ff5e1ed212a4dfd
M1NSIK-KIM <76109508+M1NSIK-KIM@users.noreply.github.com>
2023-02-03 15:24:31 +0900

    Update Exercise_Q5_Answer.py

--- 20230203/Exercise_Q5_Answer.py
+++ 20230203/Exercise_Q5_Answer.py
@@ -1,10 +1,8 @@
 def solution(n, s):
     answer = []
     if s < n: return [-1]
-    while n > 1:
+    while n > 0:
         answer.append(s//n)
         s -= s//n
         n -= 1
-        answer.append(s)
-    answer = sorted(answer)
     return answer
